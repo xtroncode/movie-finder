b044970adca41c05d7dd803aaaa2be35b2539351
Meet Shah <meet.shah@aepl.com>
2017-09-06 19:59:00 +0530

    WIP: refactor webpack config

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -1,13 +1,55 @@
-const path = require('path');
+const { join, sep } = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const joinPath = p => join(__dirname, '..', p);
 
-var commonLoaders = [
+
+// All the paths needed across the config
+const PATHS = {
+      NODE_MODULES: joinPath('node_modules'),
+      SRC: joinPath('src'),
+      APP: joinPath(`src${sep}app`),
+      CLIENT: joinPath(`src${sep}client`),
+      SERVER: joinPath(`src${sep}server`),
+      WEBPACK: joinPath('webpack'),
+      PUBLIC: joinPath('public'),
+      BUILD: joinPath('build')
+}
+// common webpack config for [ client, server][ dev, prod]
+const commonConfig = {
+      context: PATHS.SRC,
+      resolve:{
+              extensions:['.js','.css','.jsx','.json'],
+              modules: [ PATHS.APP, PATHS.CLIENT, PATHS.SERVER, PATHS.NODE_MODULES ]
+      }
+}
+
+// common webpack loaders for [ client, server][ dev, prod]
+const commonLoaders = [
     {
       test: /\.json$/,
       loader: 'json-loader'
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: Object.assign(babelConfig(true), {
+          cacheDirectory: true
+        })
+      }
     }
 ];
+
+// common webpack config for [ client ][ dev, prod]
+const clientCommon = Object.assign( {}, commonConfig, {
+
+})
+
+// common webpack config for [ server ][ dev, prod]
+const serverCommon = Object.assign( {}, commonConfig, {
+
+})
 
 module.exports = [
     {
